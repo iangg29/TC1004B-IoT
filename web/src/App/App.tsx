37b3f51df5ec1563d8264641f6b6c20866e55b35
Ian García <iangarciag@outlook.com>
2021-11-09 23:54:24 -0600

    Finished initial structure

--- web/src/App/App.tsx
+++ web/src/App/App.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import './App.css';
 
 function App() {
     return (
-        <div className="App">
-            <h2>Hello</h2>
+        <div className="text-3xl">
+            <h2>Internet of Things</h2>
         </div>
     );
 }
