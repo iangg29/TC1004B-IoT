34c9c0e4528ba6883f58ee0a09a3d8dc1532a5ce
Ian García <iangarciag@outlook.com>
2021-11-18 15:22:01 -0600

    Implemented all pending features.
    Enabled LED, status RGB and turn on/off button.

--- nodemcu/nodemcu.ino
+++ nodemcu/nodemcu.ino
@@ -24,69 +24,104 @@
 
 #define DHTTYPE DHT11
 #define dht_dpin 0
-DHT dht(dht_dpin, DHTTYPE); 
 
-const char* ssid = "network";
-const char* password = "IansHotspot29";
+DHT dht(dht_dpin, DHTTYPE);
 
-const char* serverName = "api.ian.software";
+const char *ssid = "network";
+const char *password = "IansHotspot29";
 
-unsigned long lastTime = 0;
-unsigned long timerDelay = 2000;
+const char *serverName = "api.ian.software";
 
-void setup(void)
-{ 
-  Serial.begin(9600);
-  dht.begin();
-  WiFi.begin(ssid, password);
+const int httpPort = 443;
 
-  while (WiFi.status() != WL_CONNECTED) {
-     delay(500);
-     Serial.print(".");
-  }
+const int LED = 14;
+const int BUTTON = 16;
 
-  Serial.println("");
-  Serial.println("WiFi connected");
+const int RED_LED = 12;
+const int GREEN_LED = 13;
+const int BLUE_LED = 15;
 
-  Serial.println(WiFi.localIP());
+int BUTTON_STATUS = 1;
+
+void setRGBRed() {
+    analogWrite(RED_LED, 255);
+    analogWrite(GREEN_LED, 0);
+    analogWrite(BLUE_LED, 0);
 }
-void loop() {
-  float h = dht.readHumidity();
-  float t = dht.readTemperature(); 
 
-  if (isnan(h) || isnan(t)) {
-    Serial.println("No se puede leer el sensor");
-    return;
-  } else {
-    if((millis() - lastTime) > timerDelay){
-      if(WiFi.status() == WL_CONNECTED){
-  
-        
-       WiFiClientSecure client;
-       const int httpPort = 443;
-       client.setInsecure();
-       if(!client.connect(serverName, httpPort)){
-        Serial.println("Connection failed.");
-        return;
-       }
-  
-        String url = "/payload/upload";
-        url += "?temperature=";
-        url += t;
-        url += "&humidity=";
-        url += h;
-        Serial.print("Requesting URL: ");
-        Serial.println(url);
-  
-        client.print(String("GET ") + url + " HTTP/1.1\r\n" +
-                 "Host: " + serverName + "\r\n" + 
-                 "Connection: close\r\n\r\n");
-        Serial.println();
-        Serial.println("Closing connection");
-      } else {
-        Serial.println("WiFi Disconnected.");
-      }
-      lastTime = millis();
+void setup(void) {
+    Serial.begin(9600);
+    dht.begin();
+
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
     }
-  }
+
+    Serial.println("");
+    Serial.println("WiFi connected");
+
+    Serial.println(WiFi.localIP());
+
+    pinMode(LED, OUTPUT);
+    pinMode(RED_LED, OUTPUT);
+    pinMode(GREEN_LED, OUTPUT);
+    pinMode(BLUE_LED, OUTPUT);
+
+    pinMode(BUTTON, INPUT);
+}
+
+void loop() {
+
+    setRGBRed();
+    BUTTON_STATUS = digitalRead(BUTTON);
+
+    if (BUTTON_STATUS == HIGH) {
+        Serial.println("APAGADO");
+        digitalWrite(LED, LOW);
+        setRGBRed();
+        return;
+    }
+
+    Serial.println("ENCENDIDO");
+    digitalWrite(LED, HIGH);
+
+    float h = dht.readHumidity();
+    float t = dht.readTemperature();
+
+    if (isnan(h) || isnan(t)) {
+        Serial.println("No se puede leer el sensor");
+        setRGBRed();
+        return;
+    } else {
+        if (WiFi.status() == WL_CONNECTED) {
+            analogWrite(RED_LED, 0);
+            analogWrite(GREEN_LED, 255);
+            analogWrite(BLUE_LED, 0);
+            WiFiClientSecure client;
+            client.setInsecure();
+            if(!client.connect(serverName, httpPort)){
+                Serial.println("Connection failed.");
+                return;
+            }
+
+            String url = "/payload/upload";
+            url += "?temperature=";
+            url += t;
+            url += "&humidity=";
+            url += h;
+            Serial.print("Requesting URL: ");
+            Serial.println(url);
+
+            client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+                         "Host: " + serverName + "\r\n" +
+                         "Connection: close\r\n\r\n");
+            Serial.println();
+            Serial.println("Closing connection");
+        } else {
+            Serial.println("WiFi Disconnected.");
+        }
+    }
 }
